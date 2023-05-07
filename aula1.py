b98bf150020be8070454d891f7a50fdd178f4b3c
Xander369 <xanderxlbr00@proton.me>
2023-05-07 15:22:44 -0300

    aula 1 adicionado

---
+++ aula1.py
@@ -0,0 +1,15 @@
+print('Minha primeira aula de python')
+#TRABALHANDO COM O COMANDO PRINT
+print(5+9)
+print('Xander ' * 5)
+print(5 * 6)
+print('5' * 10)
+#TRABALHANDO COM CONCATENAÇÂO
+nome = 'Xander'
+print(nome)
+nome = 'Lucas'
+print(nome)
+print('Parabéns pra você,', end='')
+print('Nesta data querida')
+print('A soma é', 10)
+print('A soma é' + '10')
