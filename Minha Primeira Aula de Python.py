6eedf87b7764654f14646ca20c2833c237d3cb7d
Xander Ricardo Lima de Medeiros <130623734+Xander369@users.noreply.github.com>
2024-10-27 19:22:25 -0300

    Create Minha Primeira Aula de Python.py

---
+++ Minha Primeira Aula de Python.py
@@ -0,0 +1,17 @@
+print('Minha primeira aula de Python')
+# TRABALHANDO COM O COMANDO PRINT
+print(5 + 9)
+print('Xander ' * 5)
+print(5 * 6)
+print('5' * 10)
+
+# TRABALHANDO COM CONCATENAÇÃO
+
+nome = 'Xander'
+print(nome)
+nome = 'Cauã'
+print(nome)
+
+print('Parabéns pra você, ', end=' ')
+print('nesta data querida.')
+print('A soma é', 10)
